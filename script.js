6af4b304afc9df1645c1bb6bdf4eab6736d9fd80
arhamfareed106 <arhamfareed575@gmail.com>
2024-10-05 20:14:39 +0500

    complete

--- script.js
+++ script.js
@@ -1,5 +1,65 @@
- // JavaScript function to toggle mobile menu
- function handleMenu() {
-    const navDialog = document.getElementById('nav-dialog');
+const navDialog = document.getElementById('nav-dialog');
+function handleMenu() {
     navDialog.classList.toggle('hidden');
 }
+
+const initialTranslateLTR = -48*4;
+const initialTranslateRTL = 36*4;
+
+function setupIntersectionObserver(element, isLTR, speed) {
+    /**
+     * @param {IntersectionObserverEntry[]} entries
+     * @param {IntersectionObserver} observer
+     */
+    const intersectionCallback = (entries, observer) => {
+        const isIntersecting = entries[0].isIntersecting;
+        if(isIntersecting) {
+            // Add scroll event listener when element comes into view
+            document.addEventListener('scroll', scrollHandler);
+        } else {
+            // Remove scroll event listener when element leaves view
+            document.removeEventListener('scroll', scrollHandler);
+        }
+    }
+    const intersectionObserver = new IntersectionObserver(intersectionCallback);
+
+    intersectionObserver.observe(element);
+
+    function scrollHandler() {
+        const translateX = (window.innerHeight - element.getBoundingClientRect().top) * speed;
+
+        let totalTranslate = 0;
+        if(isLTR) {
+            totalTranslate = translateX + initialTranslateLTR;
+        } else {
+            totalTranslate = -(translateX + initialTranslateRTL);
+        }
+
+        element.style.transform = `translateX(${totalTranslate}px)`;
+    }
+
+}
+
+const line1 = document.getElementById('line1');
+const line2 = document.getElementById('line2');
+const line3 = document.getElementById('line3');
+const line4 = document.getElementById('line4');
+
+
+setupIntersectionObserver(line1, true, 0.15);
+setupIntersectionObserver(line2, false, 0.15);
+setupIntersectionObserver(line3, true, 0.15);
+
+setupIntersectionObserver(line4, true, 0.8);
+
+const dtElements = document.querySelectorAll('dt');
+dtElements.forEach(element => {
+    element.addEventListener('click', () => {
+        const ddId = element.getAttribute('aria-controls');
+        const ddElement = document.getElementById(ddId);
+        const ddArrowIcon = element.querySelectorAll('i')[0];
+
+        ddElement.classList.toggle('hidden');
+        ddArrowIcon.classList.toggle('-rotate-180');
+    })
+})
